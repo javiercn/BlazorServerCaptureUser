2e34b745fa619618176e3da859615c18e79735a0
jacalvar <jacalvar@microsoft.com>
2022-12-22 11:38:30 +0100

    Demo how to capture the user

--- BlazorServerCaptureUser/Program.cs
+++ BlazorServerCaptureUser/Program.cs
@@ -1,11 +1,11 @@
 using BlazorServerCaptureUser.Areas.Identity;
 using BlazorServerCaptureUser.Data;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Server.Circuits;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +20,9 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddScoped<UserService>();
+builder.Services.TryAddEnumerable(ServiceDescriptor.Scoped<CircuitHandler, UserCircuitHandler>());
 
 var app = builder.Build();
 
@@ -48,3 +51,76 @@ app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
 app.Run();
+
+public class UserService
+{
+    private ClaimsPrincipal _currentUser = new ClaimsPrincipal(new ClaimsIdentity());
+
+    public ClaimsPrincipal GetUser()
+    {
+        return _currentUser;
+    }
+
+    internal void SetUser(ClaimsPrincipal user)
+    {
+        // This might be called more than once
+        if (_currentUser != user)
+        {
+            _currentUser = user;
+        }
+    }
+}
+
+internal sealed class UserCircuitHandler : CircuitHandler, IDisposable
+{
+    private readonly AuthenticationStateProvider _authenticationStateProvider;
+    private readonly UserService _userService;
+
+    public UserCircuitHandler(
+        AuthenticationStateProvider authenticationStateProvider,
+        UserService userService)
+    {
+        _authenticationStateProvider = authenticationStateProvider;
+        _userService = userService;
+    }
+
+    public void Dispose()
+    {
+        _authenticationStateProvider.AuthenticationStateChanged -= AuthenticationChanged;
+    }
+
+    // This and the method below are only needed if you want to update the user after the app has re-connected.
+    public override Task OnCircuitOpenedAsync(Circuit circuit, CancellationToken cancellationToken)
+    {
+        // If you want to update the service, then you can attach an event handler as follows
+        _authenticationStateProvider.AuthenticationStateChanged += AuthenticationChanged;
+        return base.OnCircuitOpenedAsync(circuit, cancellationToken);
+    }
+
+    private void AuthenticationChanged(Task<AuthenticationState> task)
+    {
+        _ = UpdateAuthentication(task);
+
+        async Task UpdateAuthentication(Task<AuthenticationState> task)
+        {
+            try
+            {
+                var state = await task;
+                _userService.SetUser(state.User);
+            }
+            catch
+            {
+                // Swallow all exceptions here since there is no way to report them
+                // (and if they come from the task, they'll be reported somewhere else).
+            }
+        }
+    }
+
+    public override async Task OnConnectionUpAsync(Circuit circuit, CancellationToken cancellationToken)
+    {
+        // This gets called every time the circuit reconnects, setting the user for the lifetime of the connection.
+        // Unless you implement updates.
+        var state = await _authenticationStateProvider.GetAuthenticationStateAsync();
+        _userService.SetUser(state.User);
+    }
+}
